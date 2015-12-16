57b2a553bf1ec3a5cb0904a38db9d8915305d758
Kurtev <fizel@abv.bg>
2015-12-16 22:03:30 +0200

    Upload some new Folders and Files

---
+++ Rpg_Game_Team_Doldur/Rpg_Game_Team_Doldur/Dependencies/TextBoxReader.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rpg_Game_Team_Doldur.Common
+{
+    public class TextBoxReader
+    {
+        public bool open;
+        public string currentText;
+        private List<string> text;
+        private int index;
+        public object sender;
+
+        public List<string> instructions;
+
+        public void OpenText(string name, object sender)
+        {
+            this.sender = sender;
+            open = true;
+            StreamReader reader = new StreamReader(@"Text\" + name + ".txt");
+            text = new List<string>();
+            index = 0;
+
+            instructions = new List<string>();
+
+            //read all the text for this string
+            while (!reader.EndOfStream)
+            {
+                text.Add(reader.ReadLine());
+            }
+
+            Next();
+
+            //remember to close
+            reader.Close();
+        }
+
+        public void Next()
+        {
+            currentText = text[index];
+
+            string i = currentText[0].ToString();
+
+            //Add instructions to list to be executed
+            if (string.Equals(i, "!"))
+            {
+                instructions.Clear();
+
+                while (index < text.Count)
+                {
+                    instructions.Add(text[index]);
+                    index++;
+                }
+                open = false;
+            }
+
+            index++;
+        }
+    }
+}
